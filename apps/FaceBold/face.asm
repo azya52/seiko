27fd3e4040e60407af457cce7f36bd2cc14ad989
azya52 <>
2017-09-11 01:37:37 +0300

    add custom watchface
    add custom watchface
    add build script
    fix axasm create temp folder
    add S_CLS and comments in uc2000.inc

--- apps/FaceBold/face.asm
+++ apps/FaceBold/face.asm
@@ -1,24 +1,38 @@
 ##define curPosL RB5	
 ##define curPosH RB6
 
-##define bigDigitValue RD0
+##define drawValue RD0
+
+##define secAppendL RC0
+##define secAppendH RC1
+##define checkSecZero RC2
 
 ##define bigDigitAdr RA2
+
+##define currentMode RA3
 
 ##define bigDigitAdr2 ((((bigDigit & 0x3FF)*2)>>8) & 0x0F)
 ##define bigDigitAdr1 ((((bigDigit & 0x3FF)*2)>>4) & 0x0F)
 ##define bigDigitAdr0 ((((bigDigit & 0x3FF)*2)   ) & 0x0F)
+
+##define dayOfWeekAdr2 ((((dayOfWeek & 0x3FF)*2)>>8) & 0x0F)
+##define dayOfWeekAdr1 ((((dayOfWeek & 0x3FF)*2)>>4) & 0x0F)
+##define dayOfWeekAdr0 ((((dayOfWeek & 0x3FF)*2)   ) & 0x0F)
+
+##define monthAdr2 ((((month & 0x3FF)*2)>>8) & 0x0F)
+##define monthAdr1 ((((month & 0x3FF)*2)>>4) & 0x0F)
+##define monthAdr0 ((((month & 0x3FF)*2)   ) & 0x0F)
 	
 	ORG 0 
-	JMP start		;start prog
+	JMP start		
 	JMP tick
 	JMP 0x11A
 	JMP 0x109
 	JMP 0x0E0
-	JMP btn_mode	;btn mode
-	JMP 0x283		;btn transm
-	JMP 0x2DF		;btn right
-	JMP 0x383		;btn left
+	JMP btn_mode	
+	JMP btn_transmit
+	JMP 0x2DF		
+	JMP 0x383		
 	JMP 0x2A9
 	JMP 0x307
 	JMP 0x39C
@@ -30,45 +44,57 @@
 
 start:
 	CLRM RB1, RB3%8
-    LDI RB2, 0xF
-    LCRB B2
+    LDI RB2, 0b1100
+    LCRB B0
+	LDI RB4, 2
 	JMP 0x227
 	
 btn_mode:
-	CPJR RB3, 0, btn_mode_startapp
-	CLRM RB1, RB3%8
-	JMP 0x243
-  btn_mode_startapp:
-	LDI RB3, 8
-	LDI RB1, 1
+	CALL check_watch_screen
 	JMP 0x243
 	
-tick:
+btn_transmit:
+	CALL check_watch_screen
+	JMP 0x283
+
+check_watch_screen:
+	LARB B0
+	LDI RB2, 0b1100
+	MVCA RB3,RB4
+	CPI RB3, 0
+	JZ btn_mode_startapp
+	LDI RB3, 0b0000
+	RET
+  btn_mode_startapp:
+	PLAI 125 
+	CALL OS_CLS
+	LDI RB3, 0b1000
+	JMP redraw
+		
+redraw:
 	LCRB B3
 	LARB B0
 	
-	MVCAM RA5,RB6
-	PLAI 37
-	STL RA6
-	STL RA5
+	;dots
+	PLAI 3
+	STLI 0xED
+	PLAI 23
+	STLI 0xE9
 	
-	PLAI 3                          ;draw
-	STLI 0xED                       ;dots
-	PLAI 23                         ;draw
-	STLI 0xE9                       ;dots
-	
-	MVCA bigDigitValue,RA0
+	;minutes
+	MVCA drawValue,RA0
 	LDI curPosL, 6
 	LDI curPosH, 0
 	CALL drawBigDigit
 	
-	MVCA bigDigitValue,RA1
+	MVCA drawValue,RA1
 	LDI curPosL, 4
 	LDI curPosH, 0
 	CALL drawBigDigit
 	
-	MVCA bigDigitValue, RA2
-	CPI bigDigitValue,10
+	;hours
+	MVCA drawValue, RA2
+	CPI drawValue,10
 	JC e1
 	PLAI 0                          ;draw
 	STLI 0xEE                       ;one
@@ -76,33 +102,80 @@ tick:
 	STLI 0xE6                       ;one
 	PLAI 20                         ;draw
 	STLI 0xEA                       ;one
-	SBI bigDigitValue, 10
+	SBI drawValue, 10
 e1:
 	LDI curPosL, 1
 	LDI curPosH, 0
 	CALL drawBigDigit
 	
-	CLRM RD4, 5
-	MVCAM RD4,RA5
+	;dayOfWeek
 	PLAI 30
-	STL RD5
-	STL RD4
+	MVCA drawValue, RA7
+	CALL drawDayOfWeek
+	LDI drawValue, 0xC
+	LDI drawValue+1, 0x2
+	STLM drawValue, (drawValue+1)%8
 	
+	;dayOfMonth
+	MVCA drawValue,RA5
+	CPJR drawValue, 0, +1
+	STL drawValue
+	MVCA drawValue,RA4
+	STL drawValue
+	
+	;Month
+	PLAI 37
+	MVCA drawValue, RA6
+	CALL drawMonth
 	JMP 0x98F
+	
+tick:
+	LCRB B3
+	LARB B0
+	;seconds
+	LDI secAppendH, 8
+	LDI secAppendL, 0
+	MVCA drawValue,RB6
+	MOV checkSecZero, drawValue
+	LDI drawValue+1, 0
+	ADM secAppendL, drawValue+1
+	ADM secAppendL, drawValue+1
+	PLAI 8
+	STLM secAppendL, secAppendH%8
+	PLAI 18
+	INC secAppendL, (secAppendL+1)%8
+	STLM secAppendL, secAppendH%8
+	
+	LDI secAppendH, 8
+	LDI secAppendL, 0
+	MVCA drawValue,RB5
+	ADD checkSecZero, drawValue
+	LDI drawValue+1, 0
+	ADM secAppendL, drawValue+1
+	ADM secAppendL, drawValue+1
+	PLAI 9
+	STLM secAppendL, secAppendH%8
+	PLAI 19
+	INC secAppendL, (secAppendL+1)%8
+	STLM secAppendL, secAppendH%8	
+	
+	CPI checkSecZero, 0
+	JZ redraw
+	JMP 0x98F
+
 	
 drawBigDigit:
 	LDI bigDigitAdr, bigDigitAdr2
 	LDI bigDigitAdr-1, bigDigitAdr1
 	LDI bigDigitAdr-2, bigDigitAdr0
 	
-	CLRM bigDigitValue+1, 2
-	ADM bigDigitAdr-2, bigDigitValue+2
-	ADM bigDigitAdr-2, bigDigitValue+2
+	CLRM drawValue+1, 2
+	ADM bigDigitAdr-2, drawValue+2
+	ADM bigDigitAdr-2, drawValue+2
 	
-	LDI bigDigitValue, 4
-	LDI bigDigitValue+1, 1
+	LDI drawValue, 4
+	LDI drawValue+1, 1
 	
-	LDI RC1,2
   drawBigDigit_loop1:
 	PLAM curPosL, curPosH%8
 	PSAM bigDigitAdr-2,bigDigitAdr%8
@@ -112,45 +185,71 @@ drawBigDigit:
 	JNC drawBigDigit_if1
 	INC curPosH, curPosH%8
   drawBigDigit_if1:
-    PLAM curPosL, curPosH%8
-    
-    ADM bigDigitAdr-2, bigDigitValue+2
-	DEC RC1,RC1%8
-	JNC drawBigDigit_loop1
+  
+    ADM bigDigitAdr-2, drawValue+2
+	CPI curPosH, 2
+	JNZ drawBigDigit_loop1
+	RET
+	
+drawDayOfWeek:
+	LDI bigDigitAdr, dayOfWeekAdr2
+	LDI bigDigitAdr-1, dayOfWeekAdr1
+	LDI bigDigitAdr-2, dayOfWeekAdr0
+	JMP drawLine3
+	
+drawMonth:
+	LDI bigDigitAdr, monthAdr2
+	LDI bigDigitAdr-1, monthAdr1
+	LDI bigDigitAdr-2, monthAdr0
+	
+drawLine3:
+	CLRM drawValue+1, 2
+	ADM bigDigitAdr-2, drawValue+2
+	ADM bigDigitAdr-2, drawValue+2
+	ADM bigDigitAdr-2, drawValue+2
+	PSAM bigDigitAdr-2,bigDigitAdr%8
+	CALL OS_PRINT0-3
 	RET
 	
 bigDigit:
-	DW 0xF0F4
-	DW 0x20F4
-	DW 0xF0F4
-	DW 0xF0F4
-	DW 0xE5E5
-	DW 0xF0F1
-	DW 0xF0F1
-	DW 0xF0F4
-	DW 0xF0F4
-	DW 0xF0F4
+	DW 0xF5EE	;0
+	DW 0x20EE	;1
+	DW 0xF5EE	;2
+	DW 0xF5EE	;3
+	DW 0xEEEE	;4
+	DW 0xF5FA	;5
+	DW 0xF5EE	;6
+	DW 0xF5EE	;7
+	DW 0xF5EE	;8
+	DW 0xF5EE	;9
 	
-	DW 0xE5E5
-	DW 0x20E5
-	DW 0xF0F2
-	DW 0x20FD
-	DW 0xECFD
-	DW 0xECF4
-	DW 0xE5F4
-	DW 0x20E5
-	DW 0xE5FD
-	DW 0xECFD
+	DW 0xE6E6	;0
+	DW 0x20E6	;1
+	DW 0xF5EA	;2
+	DW 0xFAE6	;3
+	DW 0xF3E6	;4
+	DW 0xF3EE	;5
+	DW 0xFEEE	;6
+	DW 0x20E6	;7
+	DW 0xFEE6	;8
+	DW 0xF3E6	;9
 	
-	DW 0xECF2
-	DW 0x20EC
-	DW 0xECF1
-	DW 0xECF2
-	DW 0x20EC	
-	DW 0xECF2
-	DW 0xECF2
-	DW 0x20EC	
-	DW 0xECF2
-	DW 0xECF2
+	DW 0xF3EA	;0
+	DW 0x20EA	;1
+	DW 0xF3EA	;2
+	DW 0xF3EA	;3
+	DW 0x20EA	;4	
+	DW 0xF3EA	;5
+	DW 0xF3EA	;6
+	DW 0x20EA	;7	
+	DW 0xF3EA	;8
+	DW 0xF3EA	;9
+	
+			
+dayOfWeek:
+	DS "SUNMONTUEWEDTHUFRISAT"
+
+month:
+	DS "JANFEBMARAPRMAYJUNJULAUGSEPOCTNOVDEC"
 	
 	END
